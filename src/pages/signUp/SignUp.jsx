443e1a28e1ca92afff8e102f3f42ff6c59207c57
asuselessbrain <shovon1023456789@gmail.com>
2024-08-15 18:09:34 +0600

    signUp page design completed

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,42 +1,70 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/Logo.png";
 
 const SignUp = () => {
+  const handleSignUp = (e) => {
+    e.preventDefault();
 
-    const handleSignUp = e =>{
-        e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const imageFile = form.image.files[0];
+    const password = form.password.value;
+    const confirmPassword = form.confirmPassword.value;
 
-        const form = e.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const phoneNumber = form.phoneNumber.value;
-        const password = form.password.value;
-        const confirmPassword = form.confirmPassword.value;
-
-        const userInfo = {
-            name,
-            email,
-            phoneNumber,
-            password,
-            confirmPassword,
-        }
-        console.log(userInfo)
-    }
+    const userInfo = {
+      name,
+      email,
+      imageFile,
+      password,
+      confirmPassword,
+    };
+    console.log(userInfo);
+  };
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-100 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <Link
           to="/"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="flex items-center mb-2 font-semibold text-gray-900 dark:text-white"
         >
-          QuickCash Mobile
+          <img className="w-36 h-36 mr-2" src={logo} alt="logo"></img>
         </Link>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create an account
-            </h1>
+            <div className="flex flex-col items-center">
+              <button className="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                <div className="bg-white p-2 rounded-full">
+                  <svg className="w-4" viewBox="0 0 533.5 544.3">
+                    <path
+                      d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                      fill="#4285f4"
+                    />
+                    <path
+                      d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                      fill="#34a853"
+                    />
+                    <path
+                      d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                      fill="#fbbc04"
+                    />
+                    <path
+                      d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                      fill="#ea4335"
+                    />
+                  </svg>
+                </div>
+                <span className="ml-4">Sign Up with Google</span>
+              </button>
+            </div>
+
+            <div className="my-12 border-b text-center">
+              <div className="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
+                Or sign up with e-mail
+              </div>
+            </div>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSignUp}>
-            <div>
+              <div>
                 <label
                   htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -73,15 +101,13 @@ const SignUp = () => {
                   htmlFor="phoneNumber"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your Phone Number
+                  Your Image
                 </label>
                 <input
-                  type="number"
-                  name="phoneNumber"
-                  id="phoneNumber"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="+8801xxxxxxxxx"
-                  required=""
+                  type="file"
+                  id="image"
+                  name="image"
+                  className="file-input w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
               <div>
@@ -115,31 +141,6 @@ const SignUp = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required=""
                 />
-              </div>
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="terms"
-                    className="font-light text-gray-500 dark:text-gray-300"
-                  >
-                    I accept the{" "}
-                    <a
-                      className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
-                </div>
               </div>
               <button
                 type="submit"
