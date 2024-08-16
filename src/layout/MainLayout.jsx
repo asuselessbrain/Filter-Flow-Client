24047d93ab0e11187b65c9c5cdc0219d7e7dcf5a
asuselessbrain <shovon1023456789@gmail.com>
2024-08-16 12:11:42 +0600

    data loaded from backend and show in card

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-[1440px] mx-auto">
             <Outlet />
         </div>
     );
