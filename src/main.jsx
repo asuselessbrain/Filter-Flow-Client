ea338ca5c49756bafacf2b024df4efa4e2f6858d
asuselessbrain <shovon1023456789@gmail.com>
2024-08-16 11:30:19 +0600

    card design completed

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router.jsx";
 import AuthProvider from "./authProvider/AuthProvider.jsx";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <ToastContainer />
     </AuthProvider>
   </React.StrictMode>
