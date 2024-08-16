632dd237ec2697846601f90f08fca4d0ccb24581
asuselessbrain <shovon1023456789@gmail.com>
2024-08-16 15:36:58 +0600

    filter by category section completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,20 +9,23 @@ const Home = () => {
   const { user, logOut } = useAuth();
   const [product, setProduct] = useState(null);
   const [productPerPage, setProductPerPage] = useState(12);
-  const [currentPage, setCurrentPage] = useState(1)
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filter, setFilter] = useState("");
 
   const { data: products = [] } = useQuery({
-    queryKey: ["products", currentPage, productPerPage],
+    queryKey: ["products", currentPage, productPerPage, filter],
     queryFn: async () => {
-      const { data } = await axios.get(`http://localhost:3000/products?page=${currentPage}&size=${productPerPage}`);
+      const { data } = await axios.get(
+        `http://localhost:3000/products?page=${currentPage}&size=${productPerPage}&filter=${filter}`
+      );
+      setProduct(data.length)
       return data;
     },
   });
 
   useEffect(() => {
     const productCount = async () => {
-      const { data } = await axios.get("http://localhost:3000/products-count/");
+      const { data } = await axios.get(`http://localhost:3000/products-count?filter=${filter}`);
       setProduct(data.count);
     };
     productCount();
@@ -38,6 +41,27 @@ const Home = () => {
   return (
     <div className="max-w-[1440px] mx-auto">
       <Searchbar />
+      <div>
+        <select
+          onChange={(e) => setFilter(e.target.value)}
+          value={filter}
+          name="category"
+          id="category"
+          className="border p-4 rounded-lg"
+        >
+          <option value="">Filter By Category</option>
+          <option value="Accessories">Accessories</option>
+          <option value="Electronics">Electronics</option>
+          <option value="Fitness">Fitness</option>
+          <option value="Furniture">Furniture</option>
+          <option value="Home Appliances">Home Appliances</option>
+          <option value="Home Automation">Home Automation</option>
+          <option value="Kitchen Appliances">Kitchen Appliances</option>
+          <option value="Outdoor">Outdoor</option>
+          <option value="Personal Care">Personal Care</option>
+          <option value="Wearables">Wearables</option>
+        </select>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
           <Card key={product._id} product={product} />
@@ -45,9 +69,10 @@ const Home = () => {
       </div>
       <div className="flex justify-center mt-12">
         <button
-        onClick={() => handleCurrentPage(currentPage-1)}
-        disabled={currentPage === 1}
-        className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
+          onClick={() => handleCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
+        >
           <div className="flex items-center -mx-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -72,16 +97,19 @@ const Home = () => {
           <button
             onClick={() => handleCurrentPage(btnNum)}
             key={btnNum}
-            className={`hidden ${currentPage === btnNum ? "bg-blue-500 text-white" : ""} px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            className={`hidden ${
+              currentPage === btnNum ? "bg-blue-500 text-white" : ""
+            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
           >
             {btnNum}
           </button>
         ))}
 
         <button
-        onClick={() => handleCurrentPage(currentPage+1)}
-        disabled={currentPage === numberOfPages}
-        className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
+          onClick={() => handleCurrentPage(currentPage + 1)}
+          disabled={currentPage === numberOfPages}
+          className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+        >
           <div className="flex items-center -mx-1">
             <span className="mx-1">Next</span>
 
